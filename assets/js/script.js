8cc46b4a994dcd5eddf887824158c8a8933453c1
Griffin <liq543@gmail.com>
2023-07-12 18:55:48 -0500

    Added inital skeleton code and tailwind

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 30.2672, lng: -97.7431 }, // Austin, Texas
+    zoom: 12,
+  });
+}
