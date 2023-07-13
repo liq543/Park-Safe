08d64237e68bade68810bd1692fed6ea8ce7c845
Griffin <liq543@gmail.com>
2023-07-13 12:33:50 -0500

    added in functions to make our code easier to use!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,81 +1,47 @@
-let map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 30.2672, lng: -97.7431 }, // Austin, Texas
-    zoom: 12,
-  });
-}
 
 
-var ajaxTime= new Date().getTime();
-$.ajax({
-  url: "https://data.austintexas.gov/resource/fdj4-gpfu.json",
-  type: "GET",
-  data: {
-    "$limit" : 5000,
-    "$$app_token" : "aDsTc4XqMfmKCL0APlSxzJlQ2",
-    // "$where": "occ_date_time between '2023-01-01T00:00:00.000' and '2023-02-01T23:59:59.000'"
-    "$where": "latitude between '30.260984' and '30.275586' AND longitude between '-97.775581' and '-97.770843'"
-  }
-}).done(function(data) {
-
-var totalTime = new Date().getTime()-ajaxTime;
-alert("Retrieved " + data.length + " records from the dataset! This took:" + totalTime);
-
-for (const crime of data) {
-  if (crime.latitude && crime.longitude) {
-    let lat = parseFloat(crime.latitude);
-    let lng = parseFloat(crime.longitude);
-
-    if (isNaN(lat) || isNaN(lng)) {
-      console.log('Invalid lat or lng:', crime.latitude, crime.longitude);
-    } else {
-      new google.maps.Marker({
-        position: { lat: lat, lng: lng },
-        map,
-        title: crime.crime_type,
-      });
-    }
-  } else {
-    console.log('Missing latitude or longitude:', crime);
-  }
-}
-console.log(data);
-});
 
 
-//Gets the latitude and longitude coordinates of the crimes
-function getCrimeCoordinates()
-{
-  $.ajax({
+function pullCrimes(latMin, latMax, lngMin, lngMax, startDate, endDate) {
+  return $.ajax({
     url: "https://data.austintexas.gov/resource/fdj4-gpfu.json",
     type: "GET",
     data: {
       "$limit" : 5000,
       "$$app_token" : "aDsTc4XqMfmKCL0APlSxzJlQ2",
-      "$where": "latitude between '30.260984' and '30.275586' AND longitude between '-97.775581' and '-97.770843'"
-
-      //Need to figure out between latitude and longitude
+      "$where": `latitude between '${latMin}' and '${latMax}' AND longitude between '${lngMin}' and '${lngMax}' AND occ_date_time between '${startDate}' and '${endDate}'`
     }
-  }).done(function(data) {
-
-    //console.log("This is Zilker Park Crimes: " + data);
-    console.log("This is how many crimes in Zilker Park " + data.length);
-
-    for(i =0; i< data.length; i++){
-  
-      var latitude = parseFloat(data[i].latitude);
-      var longitude = parseFloat(data[i].longitude);
-      var crimeList = parseFloat(data.length);
-
-
-    }
-    
   });
-
 }
 
+
+function populateMap(data) {
+  var ajaxTime = new Date().getTime();
+
+
+
+  for (const crime of data) {
+    if (crime.latitude && crime.longitude) {
+      let lat = parseFloat(crime.latitude);
+      let lng = parseFloat(crime.longitude);
+
+      if (isNaN(lat) || isNaN(lng)) {
+        console.log('Invalid lat or lng:', crime.latitude, crime.longitude);
+      } else {
+        new google.maps.Marker({
+          position: { lat: lat, lng: lng },
+          map,
+          title: crime.crime_type,
+        });
+      }
+    } else {
+      console.log('Missing latitude or longitude:', crime);
+    }
+  }
+}
+
+// Call the functions.
+pullCrimes('30.259585', '30.277721', '-97.780467', '-97.763959', '2023-06-01T00:00:00.000', '2023-06-30T23:59:59.000').done(data => populateMap(data));
 
 // getCrimeCoordinates();
 
@@ -84,3 +50,6 @@ function getCrimeCoordinates()
 
 //Zilker Park Bottom Right Corner - 30.260984, -97.770843
 //Zilker Park Top Left Corner - 30.275586, -97.775581
+
+// -97.780467
+// -97.763959
