d4078eb76abb6bc7439ff0153aab10d2c33ba47a
Griffin <liq543@gmail.com>
2023-07-14 13:22:50 -0500

    Tried to merge stuff properly probably screwed stuff up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,25 @@
-
 var list = document.getElementById("zilker-park-list");
 let map;
-accordionButton = document.querySelectorAll("button")
+var accordionButton = document.querySelectorAll("button");
 
-//Rewrote the button clicks to be the current 3 that we have. Will need to add an id for each one. (Top starts at 1 and then it keeps going down.) Can probably make a list and append it with each having an ID.
-
-accordionButton.forEach(function(button){
-  button.addEventListener("click", function(event){
+accordionButton.forEach(function(button) {
+  button.addEventListener("click", function(event) {
     console.log("button clicked");
     var buttonId = "accordion-collapse-body-" + event.target.id;
     console.log("This is the button ID:" + buttonId);
     var element = document.getElementById(buttonId);
 
     var visibility = element.classList.value;
-    
+
     console.log(visibility);
 
-    if(visibility == "visible")
-    {
+    if (visibility == "visible") {
       element.classList.value = "hidden";
-      
-    }
-    else if(visibility == "hidden")
-    {
+    } else if (visibility == "hidden") {
       element.classList.value = "visible";
     }
-  })
-})
-
+  });
+});
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
@@ -41,62 +33,28 @@ function pullCrimes(latMin, latMax, lngMin, lngMax, startDate, endDate) {
     url: "https://data.austintexas.gov/resource/fdj4-gpfu.json",
     type: "GET",
     data: {
-      "$limit" : 5000,
-      "$$app_token" : "aDsTc4XqMfmKCL0APlSxzJlQ2",
-      "$where": `latitude between '${latMin}' and '${latMax}' AND longitude between '${lngMin}' and '${lngMax}' AND occ_date_time between '${startDate}' and '${endDate}'`
-    }
+      "$limit": 5000,
+      "$$app_token": "aDsTc4XqMfmKCL0APlSxzJlQ2",
+      "$where": `latitude between '${latMin}' and '${latMax}' AND longitude between '${lngMin}' and '${lngMax}' AND occ_date_time between '${startDate}' and '${endDate}'`,
+    },
   }).done(function(data) {
+    console.log("This is how many crimes in Zilker Park " + data.length);
+    console.log(data);
 
-// async function placeSearch() {
-//   try {
-//       const searchParams = new URLSearchParams({
-//         query: 'park',
-//         ll: '30.2672,-97.7431',
-//         open_now: 'true',
-//         sort: 'DISTANCE'
-//       });
-//       const results = await fetch(
-//         `https://api.foursquare.com/v3/places/search?${searchParams}`,
-//         {
-//           method: 'GET',
-//           headers: {
-//             Accept: 'application/json',
-//             Authorization: 'YOUR ACCESS TOKEN',
-//           }
-//         }
-//       );
-//       const data = await results.json();
-//       return data;
-//   } catch (err) {
-//       console.error(err);
-//   }
-// }
+    for (i = 0; i < data.length; i++) {
+      var li = document.createElement("li");
+      var latitude = parseFloat(data[i].latitude);
+      var longitude = parseFloat(data[i].longitude);
+      var crimeList = parseFloat(data.length);
+      var crimeDateAndType = data[i].occ_date_time + " " + data[i].crime_type;
 
+      li.textContent = crimeDateAndType;
+      list.appendChild(li);
+    }
 
-   // console.log("This is how many crimes in Zilker Park " + data.length);
-    // console.log(data);
-
-    // for(i =0; i< data.length; i++){
-
-    //   var li = document.createElement("li");
-    //   var latitude = parseFloat(data[i].latitude);
-    //   var longitude = parseFloat(data[i].longitude);
-    //   var crimeList = parseFloat(data.length);
-    //   var crimeDateAndType =  data[i].occ_date_time + " " + data[i].crime_type;
-
-    //   li.textContent = crimeDateAndType;
-    //   list.appendChild(li);
-
-
-    
-
-     // console.log("This is the both " + crimeDateAndType);
-  
-  
-    
-    
-//}
-
+    populateMap(data);
+  });
+}
 
 function populateMap(data) {
   var ajaxTime = new Date().getTime();
@@ -107,7 +65,7 @@ function populateMap(data) {
       let lng = parseFloat(crime.longitude);
 
       if (isNaN(lat) || isNaN(lng)) {
-        console.log('Invalid lat or lng:', crime.latitude, crime.longitude);
+        console.log("Invalid lat or lng:", crime.latitude, crime.longitude);
       } else {
         // Create a marker
         let marker = new google.maps.Marker({
@@ -122,40 +80,40 @@ function populateMap(data) {
 
         // Create an InfoWindow
         let infoWindow = new google.maps.InfoWindow({
-          content: `<h3>Crime Committed: ${crime.crime_type}</h3><p>Date Committed: ${formattedDate}</p>`
+          content: `<h3>Crime Committed: ${crime.crime_type}</h3><p>Date Committed: ${formattedDate}</p>`,
         });
 
         // Add a click listener to the marker to open the InfoWindow
-        marker.addListener('click', function() {
+        marker.addListener("click", function() {
           infoWindow.open(map, marker);
         });
       }
     } else {
-      console.log('Missing latitude or longitude:', crime);
+      console.log("Missing latitude or longitude:", crime);
     }
   }
 }
 
-
 // Call the functions.
-pullCrimes('30.259585', '30.277721', '-97.780467', '-97.763959', '2023-06-01T00:00:00.000', '2023-06-30T23:59:59.000').done(data => populateMap(data));
+pullCrimes(
+  "30.259585",
+  "30.277721",
+  "-97.780467",
+  "-97.763959",
+  "2023-06-01T00:00:00.000",
+  "2023-06-30T23:59:59.000"
+);
 
-
-
-  
-
-
-
-var AustinParks = [zilkerPark, ladyBirdLake, bartonCreekGreenbelt, mcKinneyFallsStatePark, emmaLongMetroPark];
-var zilkerPark = ["Zilker Park", "30.2669","-97.7728"];
+var zilkerPark = ["Zilker Park", "30.2669", "-97.7728"];
 var ladyBirdLake = ["Lady Bird Lake", "30.2649", "-97.7471"];
 var bartonCreekGreenbelt = ["Barton Creek Greenbelt", "30.2619", "-97.7953"];
 var mcKinneyFallsStatePark = ["McKinney Falls State Park", "30.1587", "-97.6920"];
 var emmaLongMetroPark = ["Emma Long Metropolitan Park", "30.3599", "-97.8270"];
 
-
-//Zilker Park Bottom Right Corner - 30.259585, -97.780467
-//Zilker Park Top Left Corner - 30.277721, -97.763959
-
-// -97.780467
-// -97.763959
+var AustinParks = [
+  zilkerPark,
+  ladyBirdLake,
+  bartonCreekGreenbelt,
+  mcKinneyFallsStatePark,
+  emmaLongMetroPark,
+];
