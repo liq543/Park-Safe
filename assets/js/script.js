e290852291bfef667fee80d217b8b1e8215be4ce
Rockojoe2 <lee.d.joey@gmail.com>
2023-07-14 15:04:53 -0500

    made a new function similar to the old accordionbutton function to target the new buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,24 @@ let map;
 var accordionButton = document.querySelectorAll("button")
 var parkList = document.getElementById("accordion-collapse");
 
+// Each park array contains: park name[0], northwest corner latitude[1], northwest corner longitude[2], southeast corner latitude[3], southeast corner longitude [4]
+var zilkerPark = ["Zilker Park", "30.276187", "-97.776181", "30.260135", "-97.771194"];
+var ladyBirdLake = ["Lady Bird Lake", "30.294497", "-97.788501", "30.242677", "-97.717318"];
+var bartonCreekGreenbelt = ["Barton Creek Greenbelt", "30.285798", "-97.827646", "30.234250", "-97.784231"];
+var mcKinneyFallsStatePark = ["McKinney Falls State Park", "30.202370", "-97.725063", "30.167640",  "-97.722854"];
+var emmaLongMetroPark = ["Emma Long Metropolitan Park", "30.356452", "-97.848282", "30.323719", "-97.826107"];
+var walCreekMetroPark = ["Walnut Creek Metropolitan Park", "30.388371", "-97.677382", "30.410843", "-97.709775"];
+var peasePark = ["Pease Park", "30.289109", "-97.756060", "30.280137", "-97.750709"];
+var royGuerreroPark = ["Roy G. Guerrero Colorado River Park", "30.250026", "-97.717269", "30.235685", "-97.690601"];
+var mayfieldPark = ["Mayfield Park and Nature Preserve", "30.314760", "-97.773018", "30.310082", "-97.768630"];
+var austinNatAndSciCent = ["Austin Nature and Science Center", "30.273219", "-97.775754", "30.270420", "-97.773399"];
+var shoalCreekGreenbelt = ["Shoal Creek Greenbelt", "30.307949", "-97.748550", "30.265098", "-97.750992"];
+var muellerLakePark = ["Mueller Lake Park", "30.298086", "-97.709049", "30.294390", "-97.702620"];
+var bullCreekGreenbelt = ["Bull Creek Greenbelt", "30.385492", "-97.784090", "30.365006", "-97.764350"]
+
+// Array of all parks:
+var AustinParks = [zilkerPark, ladyBirdLake, bartonCreekGreenbelt, mcKinneyFallsStatePark, emmaLongMetroPark, walCreekMetroPark, peasePark, royGuerreroPark, mayfieldPark,austinNatAndSciCent, shoalCreekGreenbelt, muellerLakePark, bullCreekGreenbelt];
+
 //Rewrote the button clicks to be the current 3 that we have. Will need to add an id for each one. (Top starts at 1 and then it keeps going down.) Can probably make a list and append it with each having an ID.
 
 accordionButton.forEach(function(button){
@@ -50,30 +68,34 @@ function pullCrimes(latMin, latMax, lngMin, lngMax, startDate, endDate) {
 
     console.log("This is how many crimes in Zilker Park " + data.length);
      console.log(data);
- 
-     for(i =0; i< data.length; i++){
+     console.log("This is Austin Parks Length " + AustinParks.length);
 
-      // parkID = i + 1;
-      // parkIDList = "list-" + parkID;
 
-      // console.log(parkIDList);
-
-      var parkCrimeList = document.getElementById("list-1");
- 
-       var li = document.createElement("li");
-       var latitude = parseFloat(data[i].latitude);
-       var longitude = parseFloat(data[i].longitude);
-       var crimeList = parseFloat(data.length);
-       var crimeDateAndType =  data[i].occ_date_time + " " + data[i].crime_type;
- 
-       li.textContent = crimeDateAndType;
-       parkCrimeList.appendChild(li);
- 
- 
-       //console.log("This is the both " + crimeDateAndType);
+     for(i = 0; i < AustinParks.length; i++)
+     {
+      
+      var parkIDList;
+      var parkID = 1;
+      parkIDList = "list-" + parkID;
+      for(i =0; i< data.length; i++){
+        console.log(parkIDList);
+  
+        var parkCrimeList = document.getElementById(parkIDList);
+   
+         var li = document.createElement("li");
+         var latitude = parseFloat(data[i].latitude);
+         var longitude = parseFloat(data[i].longitude);
+         var crimeList = parseFloat(data.length);
+         var crimeDateAndType =  data[i].occ_date_time + " " + data[i].crime_type;
+   
+         li.textContent = crimeDateAndType;
+         parkCrimeList.appendChild(li);
    
    
+         //console.log("This is the both " + crimeDateAndType);
+       }
      }
+ 
      
    });
  }
@@ -116,23 +138,6 @@ function populateMap(data) {
   }
 }
 
-// Each park array contains: park name[0], northwest corner latitude[1], northwest corner longitude[2], southeast corner latitude[3], southeast corner longitude [4]
-var zilkerPark = ["Zilker Park", "30.276187", "-97.776181", "30.260135", "-97.771194"];
-var ladyBirdLake = ["Lady Bird Lake", "30.294497", "-97.788501", "30.242677", "-97.717318"];
-var bartonCreekGreenbelt = ["Barton Creek Greenbelt", "30.285798", "-97.827646", "30.234250", "-97.784231"];
-var mcKinneyFallsStatePark = ["McKinney Falls State Park", "30.202370", "-97.725063", "30.167640",  "-97.722854"];
-var emmaLongMetroPark = ["Emma Long Metropolitan Park", "30.356452", "-97.848282", "30.323719", "-97.826107"];
-var walCreekMetroPark = ["Walnut Creek Metropolitan Park", "30.388371", "-97.677382", "30.410843", "-97.709775"];
-var peasePark = ["Pease Park", "30.289109", "-97.756060", "30.280137", "-97.750709"];
-var royGuerreroPark = ["Roy G. Guerrero Colorado River Park", "30.250026", "-97.717269", "30.235685", "-97.690601"];
-var mayfieldPark = ["Mayfield Park and Nature Preserve", "30.314760", "-97.773018", "30.310082", "-97.768630"];
-var austinNatAndSciCent = ["Austin Nature and Science Center", "30.273219", "-97.775754", "30.270420", "-97.773399"];
-var shoalCreekGreenbelt = ["Shoal Creek Greenbelt", "30.307949", "-97.748550", "30.265098", "-97.750992"];
-var muellerLakePark = ["Mueller Lake Park", "30.298086", "-97.709049", "30.294390", "-97.702620"];
-var bullCreekGreenbelt = ["Bull Creek Greenbelt", "30.385492", "-97.784090", "30.365006", "-97.764350"]
-
-// Array of all parks:
-var AustinParks = [zilkerPark, ladyBirdLake, bartonCreekGreenbelt, mcKinneyFallsStatePark, emmaLongMetroPark, walCreekMetroPark, peasePark, royGuerreroPark, mayfieldPark,austinNatAndSciCent, shoalCreekGreenbelt, muellerLakePark, bullCreekGreenbelt];
 
 //For this function, we used chatGPT to help us make it
 function makeParkList() {
@@ -190,6 +195,31 @@ function makeParkList() {
       accordion.appendChild(heading);
       accordion.appendChild(div);
   }
+  attachButtonListeners();
+}
+
+//Used accordionButton function which took existing buttons and made them clickable, and made this function to make the new buttons made clickable.
+function attachButtonListeners() {
+  var accordionButtons = document.querySelectorAll("button");
+
+  accordionButtons.forEach(function(button) {
+    button.addEventListener("click", function(event) {
+      console.log("button clicked");
+      var buttonId = "accordion-collapse-body-" + event.target.id;
+      console.log("This is the button ID: " + buttonId);
+      var element = document.getElementById(buttonId);
+
+      var visibility = element.classList.value;
+
+      console.log(visibility);
+
+      if (visibility === "visible") {
+        element.classList.value = "hidden";
+      } else if (visibility === "hidden") {
+        element.classList.value = "visible";
+      }
+    });
+  });
 }
 
 // Call the functions.
